8fbf466f9bb9370a9ccb68cc2fb5fb3121891f79
DaviBarretoo <davibarretojkda@gmail.com>
2024-10-08 17:39:51 -0300

    Desafio finalizado

--- src/Contador.java
+++ src/Contador.java
@@ -1,2 +1,31 @@
+import java.util.Scanner;
+
 public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número: ");
+        int parametro1 = terminal.nextInt();
+        System.out.println("Digite o segundo número: ");
+        int parametro2 = terminal.nextInt();
+
+        try {
+contar(parametro1,parametro2);
+        }catch (InvalidNumberException e){
+System.out.println("O segundo parâmetro deve ser maior que o primeiro20");
+        }
+    }
+static void contar(int parametro1, int parametro2) throws InvalidNumberException{
+        if(parametro1 > parametro2){
+            throw new InvalidNumberException();
+        }
+int contagem =  parametro2 - parametro1;
+
+for(int N = 0; N <= contagem; N ++){
+    if(N == 0 ){
+        continue;
+    }
+    System.out.println("Imprimindo o número" + N);
+}
+}
 }
