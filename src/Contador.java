dfe65e2a85c5adb29656c689a96f8344d15303b3
DaviBarretoo <davibarretojkda@gmail.com>
2024-10-08 17:43:51 -0300

    Update na formatação e README.MD
    Ate aqui nos ajudu o senhor :)

--- src/Contador.java
+++ src/Contador.java
@@ -10,22 +10,23 @@ public class Contador {
         int parametro2 = terminal.nextInt();
 
         try {
-contar(parametro1,parametro2);
-        }catch (InvalidNumberException e){
-System.out.println("O segundo parâmetro deve ser maior que o primeiro20");
+            contar(parametro1, parametro2);
+        } catch (InvalidNumberException e) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro20");
         }
     }
-static void contar(int parametro1, int parametro2) throws InvalidNumberException{
-        if(parametro1 > parametro2){
+
+    static void contar(int parametro1, int parametro2) throws InvalidNumberException {
+        if (parametro1 > parametro2) {
             throw new InvalidNumberException();
         }
-int contagem =  parametro2 - parametro1;
+        int contagem = parametro2 - parametro1;
 
-for(int N = 0; N <= contagem; N ++){
-    if(N == 0 ){
-        continue;
+        for (int N = 0; N <= contagem; N++) {
+            if (N == 0) {
+                continue;
+            }
+            System.out.println("Imprimindo o número" + N);
+        }
     }
-    System.out.println("Imprimindo o número" + N);
-}
-}
 }
